6e0c5adf6632a4f5a2a144a3ea6b63725bb49c25
DasunNirmal <dasun200301@gmail.com>
2024-06-21 20:04:29 +0530

    Date and Time

--- src/main/java/lk/ijse/controller/DashBoardController.java
+++ src/main/java/lk/ijse/controller/DashBoardController.java
@@ -1,16 +1,47 @@
 package lk.ijse.controller;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.util.Duration;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class DashBoardController {
+
+    @FXML
+    private TextField txtDescription;
 
     @FXML
     private Label lblDate;
 
     @FXML
     private Label lblTime;
+
+    public void initialize() {
+        setDateAndTime();
+        txtDescription.requestFocus();
+    }
+
+    private void setDateAndTime(){
+        Platform.runLater(() -> {
+            lblDate.setText(String.valueOf(LocalDate.now()));
+
+            Timeline timeline = new Timeline(new KeyFrame(Duration.millis(1), event -> {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+                String timeNow = LocalTime.now().format(formatter);
+                lblTime.setText(timeNow);
+            }));
+            timeline.setCycleCount(Timeline.INDEFINITE);
+            timeline.play();
+        });
+    }
 
     @FXML
     void btnAddToCartOnAction(ActionEvent event) {
