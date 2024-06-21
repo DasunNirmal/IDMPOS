c5ca8a38a0e614b4eeee08012f76845050b4bd82
DasunNirmal <dasun200301@gmail.com>
2024-06-21 19:45:03 +0530

    Buttons and Lables Added

--- src/main/java/lk/ijse/controller/DashBoardController.java
+++ src/main/java/lk/ijse/controller/DashBoardController.java
@@ -1,4 +1,39 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class DashBoardController {
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    void btnAddToCartOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
 }
